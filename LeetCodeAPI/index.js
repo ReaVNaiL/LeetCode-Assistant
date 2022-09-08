46c817f8d8a6080dc587f638211a9209bbf22faa
SoRRoW.DaDa <59776018+ReaVNaiL@users.noreply.github.com>
2022-09-08 19:18:29 -0400

    Added Basic Logger To Server.

--- LeetCodeAPI/index.js
+++ LeetCodeAPI/index.js
@@ -1,15 +1,17 @@
-const app = require('express')();
+const express = require('express');
+const app = express();
 const PORT = 50520;
+
+// Add JSON body parsing middleware
+app.use(express.json());
+
+// Create logger for each request to the server with Error handling
+app.use((req, res, next) => {
+    console.log(`[${new Date().toLocaleString()}]: [${req.method}] ${req.url == '/' ? 'https://localhost:50520' : req.url}`);
+    next();
+});
 
 app.listen(
     PORT,
     () => console.log(`Server listening on port: ${PORT}`)
 )
-
-// Language: javascript
-// Set up the API logger for each request
-app.use((req, res, next) => {
-    app.log(`${req.method} ${req.url == '/' ? 'https://localhost:50520/' : req.url}`);
-    app.log(req.headers);
-    next();
-});
