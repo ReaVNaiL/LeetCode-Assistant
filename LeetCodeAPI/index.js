4eb2e9db913ac5c7fd620cc4035847c4bf77918e
SoRRoW.DaDa <59776018+ReaVNaiL@users.noreply.github.com>
2022-09-09 19:00:50 -0400

    Add Test Cases.

--- LeetCodeAPI/index.js
+++ LeetCodeAPI/index.js
@@ -2,8 +2,25 @@ const express = require('express');
 const app = express();
 const PORT = 50520;
 
+let courses = {
+    1: {
+        id : 1,
+        name : "course1"
+    },
+    2: {
+        id : 2,
+        name : "course2"
+    },
+};
+
 // Add JSON body parsing middleware
 app.use(express.json());
+
+// Test endpoint
+app.get("/test/:testvar", (req, res) => {
+    res.send(`${JSON.stringify(courses)}`);
+    // res.send(`Hello World! Your number is ${req.params.testvar}`);
+});
 
 // Create logger for each request to the server with Error handling
 app.use((req, res, next) => {
@@ -13,5 +30,5 @@ app.use((req, res, next) => {
 
 app.listen(
     PORT,
-    () => console.log(`Server listening on port: ${PORT}`)
+    () => console.log(`Server listening on https://localhost:${PORT}/`)
 )
