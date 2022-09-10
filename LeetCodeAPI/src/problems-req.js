5cf0b00b7be0c25a4eb641721f7b21f9bce8baf5
SoRRoW.DaDa <59776018+ReaVNaiL@users.noreply.github.com>
2022-09-10 14:42:41 -0400

    Refactored Data And Data Retrieval Functions

--- LeetCodeAPI/src/problems-req.js
+++ LeetCodeAPI/src/problems-req.js
@@ -1,9 +1,15 @@
-const data = require("./leetcode-data.json");
+const problems = require("../data/leetcode-data.json").stat_status_pairs;
+const problemCount = problems.length;
 
-function printElement(index){
-    lcProblem = data.stat_status_pairs[index].stat.question__title_slug;
-    console.log(lcProblem);
-    return `https://www.leetcode.com/problems/${lcProblem}`;
-};
+function printElement(index) {
+    let lcProblem = "";
+    if (index >= 0 && index < problemCount) {
+        lcProblem = problems[index].stat.question__title_slug;
+        console.log(lcProblem);
+        return `https://www.leetcode.com/problems/${lcProblem}`;
+    }
+    console.log("Index out of range");
+    return "Problem not found, please try a different index.";
+}
 
 module.exports.printElement = printElement;
