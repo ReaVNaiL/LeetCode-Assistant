c8828a441387666423ad1f2abe659aed0050ca01
SoRRoW.DaDa <59776018+ReaVNaiL@users.noreply.github.com>
2022-09-11 16:47:09 -0400

    Adjust Formatting Settings For Semicolons and Quotes

--- LeetCodeAPI/src/api/routes/problems.js
+++ LeetCodeAPI/src/api/routes/problems.js
@@ -1,34 +1,39 @@
 // Import Modules
-const settings = require("../../config/api-settings.json");
+const settings = require('../../config/api-settings.json');
 const axios = require('axios');
-const express = require("express");
+const express = require('express');
 const router = express.Router();
 
 // Import problems logic.
-const problems = require("../services/problems-req");
+const problems = require('../services/problems-req');
 
-// Get URL for given Index Problem 
-router.get("/search", (req, res) => {
+// Get URL for given Index Problem
+router.get('/search', (req, res) => {
     let data = problems.printElement(req.query.index);
     res.send(`${JSON.stringify(data)}`);
 });
 
 // Get Direct Problem List
-router.get("/refresh", (req, res) => {
+router.get('/refresh', (req, res) => {
     let session = req.query.userSession;
-    axios.request({
-        url: "https://leetcode.com/api/problems/all/",
-        method: "get",
-        headers: {
-            Cookie: session == "enabled" ? `LEETCODE_SESSION=${settings.LEETCODE_SESSION};` : ``
-        } 
-    }).then((response) => {
-        res.send(response.data);
-        console.log(response.data);
-    });
+    axios
+        .request({
+            url: 'https://leetcode.com/api/problems/all/',
+            method: 'get',
+            headers: {
+                Cookie:
+                    session == 'enabled'
+                        ? `LEETCODE_SESSION=${settings.LEETCODE_SESSION};`
+                        : ``,
+            },
+        })
+        .then((response) => {
+            res.send(response.data);
+            console.log(response.data);
+        });
 });
 
-router.get("/all", (req, res) => {
+router.get('/all', (req, res) => {
     res.send(problems.arrangeProblemSets());
 });
 
