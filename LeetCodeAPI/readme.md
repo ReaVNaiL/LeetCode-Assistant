8abdaacde7a522b0574c85f510889fe1edad7f40
Daniel Maya <59776018+ReaVNaiL@users.noreply.github.com>
2022-09-12 13:49:34 -0400

    PR2: Basic Layout For API With 3 Functional LCProblems Endpoints (#5)
    * Add Git Ignore For Node Modules.
    
    * Added Basic Logger To Server.
    
    * Add Test Cases.
    
    * Add Axio For Web Scraping
    
    * Create Placeholders For JWT Parsing
    
    * Add Readme To Api Files.
    
    * Add Test Endpoint For Retrieving Title URL
    
    * Updated EndPoint Logic
    
    * Refactored Code Test -- None Very Specific Yet.
    
    * Update TODO List in Readme.md
    
    * Refactored Data And Data Retrieval Functions
    
    * Refactored Code To Have Separate Routing
    
    * Add Base Model For Object Creation Of Given Problem
    
    * Separate Problem Arrays With Completed/Unsolved Problems
    
    * Removed Placeholer in Index.js
    
    * Completed Json File Generation For Sorted Problems
    
    * Update JSON Formatting When Exporting Data
    
    * Unused Packages Removed
    
    * Rearrange Folder Structure
    
    * Adjust Formatting Settings For Semicolons and Quotes

---
+++ LeetCodeAPI/readme.md
@@ -0,0 +1,6 @@
+# Path Plan For API Development:
+
+# TODO:
+- Refactor Data, Endpoints and App.js(index.js) For Better Separation of Concerns.
+- Figure Out How to Request/Update Session JWT Token.
+- Send Request and Get Data on API To Get Problems
---
+++ LeetCodeAPI/src/api/services/problems-req.js
@@ -0,0 +1,88 @@
+// Modules import
+const problems = require('../data/leetcode-data.json').stat_status_pairs;
+const problemCount = problems.length;
+const fs = require('fs');
+
+// index 0 = easy, 1 = medium, 2 = hard;
+let problemList = [[], [], []];
+let completedProblemList = [[], [], []];
+
+function printElement(index) {
+    let lcProblem = '';
+    if (index >= 0 && index < problemCount) {
+        lcProblem = problems[index];
+        let newProblem = createBaseModel(lcProblem);
+        return newProblem;
+    }
+    return 'Problem not found, please try a different index.';
+}
+
+function arrangeProblemSets() {
+    problemList = [[], [], []];
+    completedProblemList = [[], [], []];
+
+    problems.forEach(function (element) {
+        let newProblem = createBaseModel(element);
+
+        // Follow arr index for each diff level.
+        let arrIndex = newProblem.difficulty - 1;
+
+        // Add to correct array and sort it on each iteration.
+        if (newProblem.isCompleted) {
+            completedProblemList[arrIndex].push(newProblem);
+            sortArray(completedProblemList, arrIndex);
+        } else {
+            problemList[arrIndex].push(newProblem);
+            sortArray(problemList, arrIndex);
+        }
+    });
+
+    generateSortedJsonFile('leetcode-data-sorted.json', {
+        completedProblemList,
+        problemList,
+    });
+
+    return { completedProblemList, problemList };
+}
+
+///
+/// Helper Functions
+///
+function createBaseModel(problemSet) {
+    let problemId = problemSet.stat.question_id;
+    let problemName = problemSet.stat.question__title;
+
+    return {
+        name: problemName,
+        problemId: problemId,
+        difficulty: problemSet.difficulty.level,
+        progress: problemSet.progress,
+        isCompleted: problemSet.status == 'ac' ? true : false,
+        questionUrl: problemSet.stat.question__title_slug,
+        isNewQuestion: problemSet.stat.is_new_question,
+        paidOnly: problemSet.paid_only,
+    };
+}
+
+function generateSortedJsonFile(filename, jsonContent) {
+    let path = `./src/api/data/${filename}`;
+    fs.writeFile(path, JSON.stringify(jsonContent, null, 4), function (err) {
+        if (err) console.log(err);
+        else console.log(`File written succesfully: ${filename}.`);
+    });
+}
+
+function sortArray(arr, index) {
+    arr[index].sort((a, b) => {
+        if (a.difficulty === b.difficulty) {
+            return a.name.localeCompare(b.name);
+        }
+        return a.difficulty - b.difficulty;
+    });
+}
+///
+/// End Of Helper Functions
+///
+
+module.exports.printElement = printElement;
+module.exports.arrangeProblemSets = arrangeProblemSets;
