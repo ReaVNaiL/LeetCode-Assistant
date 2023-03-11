b09ba2fcde9eac26e78d5e36bf20a0d24e7954ad
Daniel Maya <danidd0213@gmail.com>
2023-03-11 17:25:18 -0500

    Update mispelling in "login"

--- discord-bot/index.js
+++ discord-bot/index.js
@@ -2,4 +2,4 @@ const config = require('./config/bot-credentials.json');
 const { discordClient } = require('./src/discordClient.js');
 
 // Stop the bot
-discordClient.loging(config.token);
+discordClient.login(config.token);
