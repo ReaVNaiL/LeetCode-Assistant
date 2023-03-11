7c4c78aa5e1ed4833ffe136efbbdc66f06ef6037
Daniel Maya <danidd0213@gmail.com>
2023-03-11 11:47:27 -0500

    Update Client

--- discord-bot/index.js
+++ discord-bot/index.js
@@ -1,2 +1,5 @@
-import { REST, Routes } from 'discord.js';
-import { token } from './config/bot-credentials.json';
+const config = require('./config/bot-credentials.json');
+const { client } = require('./discordClient.js');
+
+// Stop the bot
+client.destroy(config.token);
