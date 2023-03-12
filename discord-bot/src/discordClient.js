26b82f0239371d34ab27a00fdaa17e4d4a919a7f
Daniel Maya <danidd0213@gmail.com>
2023-03-11 19:36:29 -0500

    Add Command Slashes

--- discord-bot/src/discordClient.js
+++ discord-bot/src/discordClient.js
@@ -10,8 +10,8 @@ function InitializeClient() {
             GatewayIntentBits.Guilds,
             GatewayIntentBits.GuildMembers,
             GatewayIntentBits.GuildMessages,
-            GatewayIntentBits.GuildMessageReactions,
-        ],
+            GatewayIntentBits.GuildMessageReactions
+        ]
     });
 
     client.on('ready', () => {
@@ -22,6 +22,28 @@ function InitializeClient() {
 
         // Set the bot commands for all guilds
         SetBotCommands(client);
+    });
+
+    client.on('interactionCreate', async (interaction) => {
+        if (!interaction.isCommand()) return;
+
+        const { commandName, options } = interaction;
+
+        if (commandName === 'get-my-daily') {
+            await interaction.reply('Here is your daily Problem!');
+        } else if (commandName === 'get-company-problems') {
+            // Display the problem url
+            const company = options.getString('company');
+            await interaction.reply(`Here are the problems for ${company}!`);
+        } else if (commandName === 'leaderboard') {
+            await interaction.reply('Here is the leaderboard!');
+        } else if (commandName === 'help') {
+            await interaction.reply('Here is the help!');
+        } else if (commandName === 'get-my-problem') {
+            await interaction.reply('Here is your problem!');
+        } else if (commandName === 'show-solution') {
+            await interaction.reply('Here is the solution!');
+        }
     });
 
     return client;
