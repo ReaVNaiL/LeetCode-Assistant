e9da6ab80db2ced7617c0dbf45080db8eb9f927f
Daniel Maya <59776018+ReaVNaiL@users.noreply.github.com>
2023-03-17 22:34:04 +0000

    Revert "7 add get daily command to bot (#9)" (#10)
    This reverts commit bd969502132b99819544bbb1e9dffa903fd1149f.

--- discord-bot/src/discordClient.js
+++ discord-bot/src/discordClient.js
@@ -1,99 +1,32 @@
-/* eslint-disable no-console */
-const { Client, GatewayIntentBits } = require('discord.js');
-const { dailyProblemStringBuilder } = require('./helpers/dailyProblem');
-
-const { SetBotCommands } = require('./settings/botCommands');
-const { SetBotStatus } = require('./settings/botStatus');
-
-/**
- * Handles all incoming interactions from the Discord API.
- * @param {Interaction} interaction - The interaction object.
- * - This object contains all the information about the interaction, be it a command,
- *  a button, a select menu, etc.
- * @returns
- */
-async function initializeBotInteractions(interaction) {
-    console.log('\n----------------------------------------');
-    console.log(`Request received from ${interaction.user.tag}!`);
-    console.log(
-        'Command name: ',
-        interaction.commandName,
-        '\nOptions: ',
-        interaction.options ? interaction.options.data : 'No options'
-    );
-
-    if (!interaction.isCommand()) return;
-
-    const { commandName, options } = interaction;
-
-    if (commandName === 'get-my-daily') {
-        // TODO NOTE: This is a placeholder for now
-        await dailyProblemStringBuilder(
-            interaction,
-            'Two Sum',
-            'Easy',
-            'https://leetcode.com/problems/two-sum/'
-        );
-    }
-
-    if (commandName === 'get-company-problems') {
-        // TODO TEST: Testing input validation
-        const company = options.getString('company');
-        await interaction.reply(`Here are the problems for ${company}!`);
-    }
-
-    if (commandName === 'leaderboard') {
-        await interaction.reply('*NOT IMPLEMENTED*');
-    } else if (commandName === 'help') {
-        await interaction.reply('*NOT IMPLEMENTED*');
-    } else if (commandName === 'get-my-problem') {
-        await interaction.reply('*NOT IMPLEMENTED*');
-    } else if (commandName === 'show-solution') {
-        await interaction.reply('*NOT IMPLEMENTED*');
-    }
-
-    console.log('Request processed!\n');
-}
-
-/**
- * Initializes the Discord client.
- * This function is called from index.js file, and is exported to be used in other files.
- * - Sets the bot status, and bot commands.
- * - Initializes the bot interactions.
- * @returns {Client} - The Discord client object.
- * @see {@link https://discord.js.org/#/docs/main/stable/class/Client|Discord.js Client}
- */
 function InitializeClient() {
+    const { Client, GatewayIntentBits } = require('discord.js');
+
     const client = new Client({
         intents: [
             GatewayIntentBits.Guilds,
             GatewayIntentBits.GuildMembers,
             GatewayIntentBits.GuildMessages,
-            GatewayIntentBits.GuildMessageReactions
-        ]
+            GatewayIntentBits.GuildMessageReactions,
+        ],
     });
 
     client.on('ready', () => {
         console.log(`Logged in as ${client.user.tag}!`);
 
-        // Set the bot status
-        SetBotStatus(client, 'Nyan Cat :3');
+        client.user.setActivity('Being a menace to society...', {
+            type: 'WATCHING',
+        });
 
-        // Set the bot commands for all guilds
-        SetBotCommands(client);
-    });
+        const commands = [
+            {
+                name: 'get-my-daily',
+                description: 'Get your daily LC problem!',
+            },
+        ];
 
-    // Initialize the bot interactions
-    client.on('interactionCreate', async (interaction) => {
-        try {
-            await initializeBotInteractions(interaction);
-        } catch (error) {
-            console.error(error);
-            await interaction.reply({
-                content: 'There was an error while executing this command!',
-                ephemeral: true
-            });
-        }
+        client.guilds.cache.forEach(async (guild) => {
+            await guild.commands.set(commands);
+        });
     });
 
     return client;
