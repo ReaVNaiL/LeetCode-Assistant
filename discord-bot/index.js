a946017fa612105cfb5591768979428112d7bebd
Daniel Maya <danidd0213@gmail.com>
2023-03-19 12:58:02 -0400

    Refactor Scripts into it's own folder.

--- discord-bot/index.js
+++ discord-bot/index.js
@@ -1,31 +1,9 @@
 const config = require('./config/bot-credentials.json');
 const { discordClient } = require('./src/discordClient');
-const { spawn } = require('child_process');
-const cron = require('node-cron');
+const { executeDeploymentSchedule } = require('./scripts/deployChildTask');
 
-// define the task to run every 24 hours
-const task = cron.schedule('0 10 * * *', () => {
-  console.log('Running git pull and pm2 restart...');
-
-  // spawn the child process
-  const child = spawn('bash', ['./LeetCode-Generator-Discord-Bot/discord-bot/git_pull_and_restart_pm2.sh']);
-
-  // handle the output and errors
-  child.stdout.on('data', (data) => {
-    console.log(`stdout: ${data}`);
-  });
-
-  child.stderr.on('data', (data) => {
-    console.error(`stderr: ${data}`);
-  });
-
-  child.on('close', (code) => {
-    console.log(`child process exited with code ${code}`);
-  });
-});
-
-// start the task
-task.start();
+// Start the deployment schedule
+executeDeploymentSchedule();
 
 // Stop the bot
 discordClient.login(config.token);
