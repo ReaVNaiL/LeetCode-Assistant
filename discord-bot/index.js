327cc455cfe899d556a3a8547f92d2d6e250940c
Daniel Maya <59776018+ReaVNaiL@users.noreply.github.com>
2023-03-19 17:46:40 +0000

    Update Deployment [Settings / Scripts] (#67)
    * pm2 test
    
    * Update NyanCat
    
    * Get Files From Main
    
    * Update package json
    
    * Refactor Scripts into it's own folder.
    
    * Disable ESLint for Script File
    
    * Update Labeler Workflow

--- discord-bot/index.js
+++ discord-bot/index.js
@@ -1,5 +1,9 @@
 const config = require('./config/bot-credentials.json');
 const { discordClient } = require('./src/discordClient');
+const { executeDeploymentSchedule } = require('./scripts/deployChildTask');
+
+// Start the deployment schedule
+executeDeploymentSchedule();
 
 // Stop the bot
 discordClient.login(config.token);
