561f9af1daed76c83d97d1abb1fd1df679d07a10
Daniel Maya <59776018+ReaVNaiL@users.noreply.github.com>
2023-03-19 23:52:44 +0000

    Lower schedule interval [TEMP] (#76)

--- packages/discord-bot/scripts/deployChildTask.js
+++ packages/discord-bot/scripts/deployChildTask.js
@@ -1,32 +1,33 @@
 /* eslint-disable */
-const { spawn } = require('child_process');
-const cron = require('node-cron');
+const { spawn } = require("child_process");
+const cron = require("node-cron");
 
 // define the task to run every 24 hours
 function executeDeploymentSchedule() {
-    const task = cron.schedule('0 10 * * *', () => {
-    const task = cron.schedule('*/15 * * * *', () => {
-        console.log('Running git pull and pm2 restart...');
+  const task = cron.schedule("*/1 * * * *", () => {
+    console.log("Running git pull and pm2 restart...");
 
-        // spawn the child process
-        const child = spawn('bash', ['~/LeetCode-Generator-Discord-Bot/packages/discord-bot/scripts/git_pull_and_restart_pm2.sh']);
+    // spawn the child process
+    const child = spawn("bash", [
+      "./LeetCode-Generator-Discord-Bot/packages/discord-bot/scripts/git_pull_and_restart_pm2.sh",
+    ]);
 
-        // handle the output and errors
-        child.stdout.on('data', (data) => {
-            console.log(`stdout: ${data}`);
-        });
-
-        child.stderr.on('data', (data) => {
-            console.error(`stderr: ${data}`);
-        });
-
-        child.on('close', (code) => {
-            console.log(`child process exited with code ${code}`);
-        });
+    // handle the output and errors
+    child.stdout.on("data", (data) => {
+      console.log(`stdout: ${data}`);
     });
 
-    // start the task
-    task.start();
+    child.stderr.on("data", (data) => {
+      console.error(`stderr: ${data}`);
+    });
+
+    child.on("close", (code) => {
+      console.log(`child process exited with code ${code}`);
+    });
+  });
+
+  // start the task
+  task.start();
 }
 
 exports.executeDeploymentSchedule = executeDeploymentSchedule;
