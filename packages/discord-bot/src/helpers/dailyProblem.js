163630df1d598efbf3ac84150b6f33e3febef882
Daniel Maya <danidd0213@gmail.com>
2023-03-21 11:13:45 -0400

    Update Cron Task Scheduler

--- packages/discord-bot/src/helpers/dailyProblem.js
+++ packages/discord-bot/src/helpers/dailyProblem.js
@@ -1,7 +1,10 @@
-// eslint-disable-next-line import/no-extraneous-dependencies
+/* eslint-disable no-console */
 const axios = require('axios');
-const problemList = require('../data/daily-list.json');
 const cron = require('node-cron');
+const problemList = require('../data/daily-list.json');
+const { getCurrentFormattedDate } = require('./timeHandler');
+
+const CRON_SCHEDULE = '0 12 * * *';
 
 /**
  * This function is used to build the string for the daily problem
@@ -40,15 +43,6 @@ async function requestProblemInfo(problemLink) {
 }
 
 /**
- * Start the task to remove the problem from the list after 24 hours
- */
-function removeProblemFromList() {
-    cron.schedule('0 12 * * *', () => {
-        delete problemList[Object.keys(problemList)[0]];
-    });
-}
-
-/**
  * Get a problem from the list of problems, and request the API for the problem details
  */
 async function getDailyProblem() {
@@ -61,6 +55,47 @@ async function getDailyProblem() {
     return problemInfo;
 }
 
-exports.dailyProblemStringBuilder = dailyProblemStringBuilder;
-exports.getDailyProblem = getDailyProblem;
-exports.removeProblemFromList = removeProblemFromList;
+/**
+ * Start the task to remove the problem from the list after 24 hours
+ */
+function removeProblemFromList() {
+    cron.schedule(CRON_SCHEDULE, () => {
+        delete problemList[Object.keys(problemList)[0]];
+    });
+}
+
+/**
+ * Send the daily problem message to the channel
+ * @param {Object} client - The Discord client
+ * @param {String} CHANNEL_ID - The channel ID
+ */
+function sendDailyProblemMessage(client, CHANNEL_ID) {
+    cron.schedule(CRON_SCHEDULE, async () => {
+        const channel = client.channels.cache.get(CHANNEL_ID);
+
+        if (channel) {
+            channel.send('Updating the daily problem...');
+            const interaction = { channel };
+
+            const daily = await getDailyProblem();
+            await dailyProblemStringBuilder(
+                interaction,
+                daily.title,
+                daily.type,
+                daily.difficulty,
+                daily.link
+            );
+
+            console.log(
+                `[${getCurrentFormattedDate()}] Daily problem updated!`
+            );
+        }
+    });
+}
+
+module.exports = {
+    dailyProblemStringBuilder,
+    getDailyProblem,
+    removeProblemFromList,
+    sendDailyProblemMessage
+};
