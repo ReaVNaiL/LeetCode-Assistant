68d6bb096853313e054c948afbb2e022d72d0f31
Daniel Maya <59776018+ReaVNaiL@users.noreply.github.com>
2023-03-21 20:08:38 +0000

    Missing Problem Count Tracker On Bots Status [1/1]
    * Add Custom Bot Status
    
    * Update problem tracker
    
    * Make Client Use Problem Tracker
    
    * Update Problem List
    
    * Update Logic for Problem Status
    
    * Add Gitignore Packages
    
    * Daily Message Update - [BUG FIX]
    
    * File Update for Daily Problems.
    
    * Add Docstrings to botStatus
    
    * Update Client + Scheduler
    
    * Add Problem Update to Client

--- packages/discord-bot/src/settings/botStatus.js
+++ packages/discord-bot/src/settings/botStatus.js
@@ -13,4 +13,31 @@ function SetBotStatus(client, status) {
     });
 }
 
-exports.SetBotStatus = SetBotStatus;
+/**
+ * Set the bot status to show the current progress
+ * @param {Client} client - The Discord client object
+ * @param {Number} count - The current progress count
+ * @see {@link https://discord.js.org/#/docs/main/stable/class/Client|Discord.js Client}
+ */
+function SetCountBotStatus(client, count) {
+    const status = `Progress ${count}/150`;
+    client.user.setPresence({
+        activities: [
+            {
+                name: status,
+                type: ActivityType.Playing,
+                details: 'Progress',
+                state: `${count}/150`,
+                timestamps: {
+                    start: Date.now()
+                }
+            }
+        ],
+        status: 'dnd'
+    });
+}
+
+module.exports = {
+    SetBotStatus,
+    SetCountBotStatus
+};
