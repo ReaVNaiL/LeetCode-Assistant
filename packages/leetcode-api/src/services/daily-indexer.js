7f99702ab4dc581f0398e0293cebf0720a936378
Daniel Maya <danidd0213@gmail.com>
2023-03-23 12:22:25 -0400

    Moved Daily Problem's Logic to an Indexer

---
+++ packages/leetcode-api/src/services/daily-indexer.js
@@ -0,0 +1,59 @@
+const problems = require('../data/daily-list.json');
+const problemReq = require('./problems-req');
+
+// Global variables
+const START_DATE = new Date('2023-03-20'); // Possibly change this to the date of the first problem
+const TOTAL_PROBLEMS = 150;
+
+/// HELPERS
+function getProblemIndex(today, startDate) {
+    const msPerDay = 24 * 60 * 60 * 1000;
+    const daysDifference = Math.floor((today - startDate) / msPerDay);
+    return daysDifference % TOTAL_PROBLEMS;
+}
+
+function getProblemForToday(onlyLink = false) {
+    const today = new Date();
+    const problemIndex = getProblemIndex(today, START_DATE);
+    return onlyLink
+        ? Object.keys(problems)[problemIndex]
+        : problems[Object.keys(problems)[problemIndex]];
+}
+
+/// EXPORTS
+function getDailyProblemType() {
+    return getProblemForToday();
+}
+
+function getDailyProblemLink() {
+    return getProblemForToday(true);
+}
+
+/**
+ * Get a problem from the list of problems, and request the API for the problem details
+ * @returns {Object} - The problem details:
+ *
+ * { `title`, `type`, `difficulty`, `link` }
+ */
+function getDailyProblem() {
+    const problemLink = getDailyProblemLink();
+    const problemType = getDailyProblemType();
+
+    return problemReq.getProblemByUrl(problemLink, problemType);
+}
+
+/**
+ * Get Current Progress List
+ * @returns {Object} Count of problems left
+ */
+function getCurrentProgressList() {
+    const today = new Date();
+    return getProblemIndex(today, START_DATE);
+}
+
+module.exports = {
+    getDailyProblemType,
+    getDailyProblemLink,
+    getDailyProblem,
+    getCurrentProgressList
+};
